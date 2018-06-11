3962d7320746d6b79724d984a29fccc068ae706c
may-my-tech <39182655+Taksapong@users.noreply.github.com>
2018-06-11 22:41:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 <html>
 <body>
 <h1>Test Taksapong-Mamon-Yuwadee</h1>
+  
+<form action="/action_page.php">
+  <label for="male">Male</label>
+  <input type="radio" name="gender" id="male" value="male"><br>
+  <label for="female">Female</label>
+  <input type="radio" name="gender" id="female" value="female"><br>
+  <label for="other">Other</label>
+  <input type="radio" name="gender" id="other" value="other"><br><br>
+  <input type="submit" value="Submit">
+</form>
 <?php
 echo "test PHP";
 ?> 
