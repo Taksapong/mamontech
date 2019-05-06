f48daa19b3c197afa50900360137a5986c8737f4
may-my-tech <39182655+Taksapong@users.noreply.github.com>
2019-05-06 13:39:13 +0700

    Update mamon.php

--- mamon.php
+++ mamon.php
@@ -4,7 +4,7 @@
 <body bgcolor=#F0FFFF>
 <h1>Test Taksapong-Mamon-Yuwadee</h1>
   <h1>Hello MAMON<h1>
-  <p>LED #1 <a href="index.php"><button>ON</button></a>&nbsp;<a onclick=\"alert('mamon')\"><button>OFF</button></a></p>
+  <p>LED #1 <a href="index.php"><button>ON</button></a>&nbsp;<button type="button" onclick="alert('Hello world!')">Click Me!</p>
 
 
 </body>
