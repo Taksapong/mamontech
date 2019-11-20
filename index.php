3eb28a0278e63f2b3f688b49c1a374b4ee1713df
may-my-tech <39182655+Taksapong@users.noreply.github.com>
2019-11-20 08:22:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <h1>Test Taksapong-Mamon-Yuwadee</h1>
   <h1>ESP32 - Web Server</h1>
   <h2><cite>MR.TAKSAPONG (MAYMYTECH)</cite></h2>
-  <h4>Relay 1 - State: " + relay1State + "</h4>
   <p>LED #1 <a href="mamon.php"><button>ON</button></a>&nbsp;<a href=\"off1\"><button>OFF</button></a></p>
   <p>LED #2 <a href=\"on2\"><button>ON</button></a>&nbsp;<a href=\"off2\"><button>OFF</button></a></p>
 
